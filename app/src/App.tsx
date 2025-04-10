d126d6040e844343fef230a831166cded955d3b1
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2025-04-10 22:50:00 +0200

    Failed to fully launch with npm run dev but will try again ! Let's do it

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
 import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Dice from './components/dice/Dice';
-import BodyDice from './components/dice/BodyDice';
+import Footer from './components/footer/footer';
+import Dice from './components/dice/dice';
+import BodyDice from './components/dice/bodyDice';
 import './styles/main.scss';
 
 const diceValues = [100, 20, 12, 10, 8, 6, 4];
 
 const App = () => {
   return (
-    <Header />
-    <main className="container">
-      {diceValues.map((dice) => (
-        <Dice key={dice} dice={dice} />
-      ))}
-      <BodyDice />
-    </main>
-    <Footer />
+    <html>
+      <Header />
+      <main className="container">
+        {diceValues.map((dice) => (
+          <Dice key={dice} dice={dice} />
+        ))}
+        <BodyDice />
+      </main>
+      <Footer />
+    </html>
   );
 };
+
+export default App;
