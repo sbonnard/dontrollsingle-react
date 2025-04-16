c384582fd994ae2c33f9aed7cb949dc05270e97a
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2025-04-16 14:07:15 +0200

    Working on worldmap adaptation

--- app/src/AppWorldMap.tsx
+++ app/src/AppWorldMap.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import Header from './components/header/Header.tsx';
 import Footer from './components/footer/footer.tsx';
-import WorldMap from './components/worldmap/worldmap.tsx';
+import WorldMap from './components/worldmap/Worldmap.tsx';
 import './styles/main.scss';
 
 const AppWorldMap = () => {
